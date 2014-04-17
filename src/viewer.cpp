65642fe37f6efd6b8f93d415ed980696564ec9ca
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-04-17 16:02:42 +0000

    Created rgbd viewer
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/code/tue-ros-pkg/trunk/rgbd_transport@23475 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

---
+++ src/viewer.cpp
@@ -0,0 +1,23 @@
+#include "rgbd_transport/Client.h"
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "rgbd_viewer");
+
+    rgbd::Client client;
+    client.intialize("rgbd");
+
+    ros::Rate r(30);
+    while (ros::ok()) {
+        rgbd::RGBDImage image;
+        if (client.nextImage(image)) {
+            cv::imshow("depth", image.getDepthImage() / 8);
+            cv::waitKey(3);
+        }
+        r.sleep();
+    }
+
+    ros::spin();
+
+    return 0;
+}
