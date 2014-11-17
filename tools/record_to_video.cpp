e1108a7b5f0a92c93f8a3aff2d2898199ce645ec
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2014-11-17 10:36:20 +0000

    Can set size, format (codec) and filename using parameters
    git-svn-id: https://roboticssrv.wtb.tue.nl/svn/ros/trunk/rgbd@28266 3a3c2cfb-d3b9-409e-9e75-db8fcdbcaac6

--- tools/record_to_video.cpp
+++ tools/record_to_video.cpp
@@ -1,64 +1,34 @@
-#include <ros/ros.h>
+// ROS
+#include <ros/init.h>
+#include <ros/node_handle.h>
+#include <ros/subscriber.h>
 
+// ROS image message
 #include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
 
-// Message filters
-#include <message_filters/synchronizer.h>
-#include <message_filters/subscriber.h>
-#include <message_filters/sync_policies/approximate_time.h>
-
+// For converting image messages to OpenCV
+#include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 
-// CV bridge
-#include <cv_bridge/cv_bridge.h>
-
+// Writing video files
 #include <opencv2/highgui/highgui.hpp>
 
-#include "rgbd/Image.h"
-#include "rgbd/Server.h"
+#include <opencv2/imgproc/imgproc.hpp>
 
-#include <image_geometry/pinhole_camera_model.h>
-
-typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> KinectApproxPolicy;
-
+// Received RGB image
 cv::Mat rgb_image;
 
 // ----------------------------------------------------------------------------------------
 
-void imageCallback(sensor_msgs::ImageConstPtr rgb_image_msg, sensor_msgs::ImageConstPtr depth_image_msg) {
+void imageCallback(const sensor_msgs::ImageConstPtr& rgb_image_msg) {
 
-    cv_bridge::CvImagePtr img_ptr, depth_img_ptr;
+    cv_bridge::CvImagePtr img_ptr;
 
-    // Convert RGB image
+    // Convert RGB image message to OpenCV mat
     try {
         img_ptr = cv_bridge::toCvCopy(rgb_image_msg, sensor_msgs::image_encodings::BGR8);
     } catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not deserialize rgb image: %s", e.what());
-        return;
-    }
-
-    // Convert depth image
-    try {
-        depth_img_ptr = cv_bridge::toCvCopy(depth_image_msg, depth_image_msg->encoding);
-
-        if (depth_image_msg->encoding == "16UC1")
-        {
-            // depths are 16-bit unsigned ints, in mm. Convert to 32-bit float (meters)
-            cv::Mat depth_image(depth_img_ptr->image.rows, depth_img_ptr->image.cols, CV_32FC1);
-            for(int x = 0; x < depth_image.cols; ++x)
-            {
-                for(int y = 0; y < depth_image.rows; ++y)
-                {
-                    depth_image.at<float>(y, x) = ((float)depth_img_ptr->image.at<unsigned short>(y, x)) / 1000; // (mm to m)
-                }
-            }
-
-            depth_img_ptr->image = depth_image;
-        }
-
-    } catch (cv_bridge::Exception& e) {
-        ROS_ERROR("Could not deserialize depth image: %s", e.what());
         return;
     }
 
@@ -70,35 +40,79 @@ void imageCallback(sensor_msgs::ImageConstPtr rgb_image_msg, sensor_msgs::ImageC
 int main(int argc, char **argv) {
     ros::init(argc, argv, "rgbd_transport_server");
 
-    ros::NodeHandle nh;
+    // Read parameters
     ros::NodeHandle nh_private("~");
 
     double video_frame_rate = 30;
     nh_private.getParam("fps", video_frame_rate);
 
-    const cv::Size2i video_size(640, 480);
-    cv::VideoWriter video_writer("/tmp/video.avi", CV_FOURCC('M','J','P','G'), video_frame_rate, video_size);
+    std::string filename;
+    nh_private.getParam("filename", filename);
 
-    if (!video_writer.isOpened())
+    std::string format = "DIVX";
+    nh_private.getParam("format", format);
+
+    double size = 1;
+    nh_private.getParam("size", size);
+
+    if (format.size() != 4)
     {
-        std::cout << "Unable to create video writer" << std::endl;
+        ROS_ERROR("Parameter 'format' should be string of size 4 (e.g., MJPG, DIVX, MPG4, etc)");
         return 1;
     }
 
-    message_filters::Subscriber<sensor_msgs::Image> sub_rgb_sync_(nh, "rgb_image", 1);
-    message_filters::Subscriber<sensor_msgs::Image> sub_depth_sync_(nh, "depth_image", 1);
+    // Subscriber to image topic
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("rgb", 1, imageCallback);
 
-    message_filters::Synchronizer<KinectApproxPolicy> sync_(KinectApproxPolicy(10), sub_rgb_sync_, sub_depth_sync_);
-    sync_.registerCallback(boost::bind(&imageCallback, _1, _2));
+    cv::VideoWriter video_writer;
+    bool initialized = false;
 
+    // video size
+    cv::Size2i video_size;
+
+    // Start loop at given frequency
     ros::Rate r(video_frame_rate);
     while (ros::ok())
     {
         ros::spinOnce();
 
+        // Check if we already received an image
         if (rgb_image.data)
-            video_writer.write(rgb_image);
+        {
+            // Check ik we already initialized the video writer
+            if (!initialized)
+            {
+                // If not, do so
+                video_size = cv::Size2i(size * rgb_image.cols, size * rgb_image.rows);
+                video_writer.open(filename.c_str(), CV_FOURCC(format[0], format[1], format[2], format[3]), video_frame_rate, video_size);
 
+                if (!video_writer.isOpened())
+                {
+                    // Could not create the video writer, so exit
+                    std::cout << "Unable to create video writer" << std::endl;
+                    return 1;
+                }
+
+                initialized = true;
+            }
+
+            if (size == 1)
+            {
+                // Write received image to the video
+                video_writer.write(rgb_image);
+            }
+            else
+            {
+                cv::Mat rgb_image_scaled;
+                cv::resize(rgb_image, rgb_image_scaled, video_size);
+
+                // Write received image to the video
+                video_writer.write(rgb_image_scaled);
+            }
+        }
+
+        // Sleep for remaining loop time
         r.sleep();
     }
 
