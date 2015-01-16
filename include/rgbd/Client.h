06ae50e7aaccd8de330dd993fd9ef31ed1b0d17f
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-01-16 16:09:38 +0100

    rgbd::Client can now also directly listen to standard ROS kinect topics

--- include/rgbd/Client.h
+++ include/rgbd/Client.h
@@ -1,3 +1,11 @@
+/**
+ * This client can either be used to listen to RGBD messages on a single topic
+ * (use initialize(_)) or to seperate RGB / Depth / camera info topics
+ * (use initialize(_, _, _)).
+ *
+ * The client will always convert the incoming data to rgbd::Image data structures.
+ */
+
 #ifndef RGBD_TRANSPORT_CLIENT_H_
 #define RGBD_TRANSPORT_CLIENT_H_
 
@@ -8,7 +16,14 @@
 
 #include "rgbd/types.h"
 
+// ROS message serialization
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+
 namespace rgbd {
+
+// Forward declare struct that holds all approximate time sync stuff
+struct ROSImageSyncData;
 
 class Image;
 
@@ -22,7 +37,9 @@ public:
 
     void intialize(const std::string& server_name);
 
-    bool initialized() { return !sub_image_.getTopic().empty(); }
+    void intialize(const std::string& rgb_image_topic, const std::string& depth_image_topic, const std::string& cam_info_topic);
+
+    bool initialized() { return !sub_image_.getTopic().empty() || ros_image_sync_data_; }
 
     bool nextImage(Image& image);
 
@@ -30,13 +47,21 @@ public:
 
 protected:
 
+    ros::NodeHandle* nh_;
+
     ros::Subscriber sub_image_;
     ros::CallbackQueue cb_queue_;
 
     bool received_image_;
     Image* image_ptr_;
 
-    void imageCallback(const rgbd::RGBDMsg::ConstPtr& msg);
+    ROSImageSyncData* ros_image_sync_data_;
+
+    void rgbdImageCallback(const rgbd::RGBDMsg::ConstPtr& msg);
+
+    void camInfoCallback(const sensor_msgs::CameraInfoConstPtr& cam_info_msg);
+
+    void imageCallback(sensor_msgs::ImageConstPtr rgb_image_msg, sensor_msgs::ImageConstPtr depth_image_msg);
 
 };
 
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,37 +1,94 @@
 #include "rgbd/Client.h"
 #include <opencv2/highgui/highgui.hpp>
 #include "rgbd/Image.h"
-#include "rgbd/serialization.h"
 
+// RGBD message serialization
+#include "rgbd/serialization.h"
 #include <tue/serialization/conversions.h>
+
+// ROS message serialization
+#include <cv_bridge/cv_bridge.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <sensor_msgs/image_encodings.h>
+#include <image_geometry/pinhole_camera_model.h>
+
+typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Image, sensor_msgs::Image> KinectApproxPolicy;
 
 namespace rgbd {
 
 // ----------------------------------------------------------------------------------------
 
-Client::Client() {
+struct ROSImageSyncData
+{
+    ROSImageSyncData() : sync_(0), sub_rgb_sync_(0), sub_depth_sync_(0) {}
+
+    ~ROSImageSyncData()
+    {
+        delete sub_rgb_sync_;
+        delete sub_depth_sync_;
+        delete sync_;
+    }
+
+    message_filters::Synchronizer<KinectApproxPolicy>* sync_;
+    message_filters::Subscriber<sensor_msgs::Image>* sub_rgb_sync_;
+    message_filters::Subscriber<sensor_msgs::Image>* sub_depth_sync_;
+    ros::Subscriber sub_cam_info_;
+    image_geometry::PinholeCameraModel cam_model_;
+};
+
+// ----------------------------------------------------------------------------------------
+
+Client::Client() : nh_(0), ros_image_sync_data_(0)
+{
 }
 
 // ----------------------------------------------------------------------------------------
 
-Client::~Client(){
+Client::~Client()
+{
+    delete nh_;
+    delete ros_image_sync_data_;
 }
 
 // ----------------------------------------------------------------------------------------
 
 void Client::intialize(const std::string& server_name) {
-    ros::NodeHandle nh;
+
+    delete nh_;
+    nh_ = new ros::NodeHandle();
 
     ros::SubscribeOptions sub_options =
             ros::SubscribeOptions::create<rgbd::RGBDMsg>(
-                server_name, 1, boost::bind(&Client::imageCallback, this, _1), ros::VoidPtr(), &cb_queue_);
+                server_name, 1, boost::bind(&Client::rgbdImageCallback, this, _1), ros::VoidPtr(), &cb_queue_);
 
-    sub_image_ = nh.subscribe(sub_options);
+    sub_image_ = nh_->subscribe(sub_options);
 }
 
 // ----------------------------------------------------------------------------------------
 
-bool Client::nextImage(Image& image) {
+void Client::intialize(const std::string& rgb_image_topic, const std::string& depth_image_topic, const std::string& cam_info_topic)
+{
+    delete nh_;
+    nh_ = new ros::NodeHandle();
+    nh_->setCallbackQueue(&cb_queue_);
+
+    ros_image_sync_data_ = new ROSImageSyncData();
+
+    ros_image_sync_data_->sub_cam_info_ = nh_->subscribe(cam_info_topic, 1, &Client::camInfoCallback, this);
+
+    ros_image_sync_data_->sub_rgb_sync_ = new message_filters::Subscriber<sensor_msgs::Image>(*nh_, rgb_image_topic, 1);
+    ros_image_sync_data_->sub_depth_sync_ = new message_filters::Subscriber<sensor_msgs::Image>(*nh_, depth_image_topic, 1);
+
+    ros_image_sync_data_->sync_ = new message_filters::Synchronizer<KinectApproxPolicy>(KinectApproxPolicy(10), *ros_image_sync_data_->sub_rgb_sync_, *ros_image_sync_data_->sub_depth_sync_);
+    ros_image_sync_data_->sync_->registerCallback(boost::bind(&Client::imageCallback, this, _1, _2));
+}
+
+// ----------------------------------------------------------------------------------------
+
+bool Client::nextImage(Image& image)
+{
     received_image_ = false;
     image_ptr_ = &image;
     cb_queue_.callAvailable();
@@ -47,8 +104,76 @@ ImagePtr Client::nextImage() {
 }
 
 // ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
 
-void Client::imageCallback(const rgbd::RGBDMsg::ConstPtr& msg) {
+void Client::imageCallback(sensor_msgs::ImageConstPtr rgb_image_msg, sensor_msgs::ImageConstPtr depth_image_msg)
+{
+    if (!ros_image_sync_data_->cam_model_.initialized())
+        return;
+
+    cv_bridge::CvImagePtr img_ptr, depth_img_ptr;
+
+    // Convert RGB image
+    try
+    {
+        img_ptr = cv_bridge::toCvCopy(rgb_image_msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not deserialize rgb image: %s", e.what());
+        return;
+    }
+
+    // Convert depth image
+    try
+    {
+        depth_img_ptr = cv_bridge::toCvCopy(depth_image_msg, depth_image_msg->encoding);
+
+        if (depth_image_msg->encoding == "16UC1")
+        {
+            // depths are 16-bit unsigned ints, in mm. Convert to 32-bit float (meters)
+            cv::Mat depth_image(depth_img_ptr->image.rows, depth_img_ptr->image.cols, CV_32FC1);
+            for(int x = 0; x < depth_image.cols; ++x)
+            {
+                for(int y = 0; y < depth_image.rows; ++y)
+                {
+                    depth_image.at<float>(y, x) = ((float)depth_img_ptr->image.at<unsigned short>(y, x)) / 1000; // (mm to m)
+                }
+            }
+
+            depth_img_ptr->image = depth_image;
+        }
+
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("Could not deserialize depth image: %s", e.what());
+        return;
+    }
+
+    if (!image_ptr_) {
+        // in this case, the pointer will always be wrapped in a shared ptr, so no mem leaks (see nextImage() )
+        image_ptr_ = new Image();
+    }
+
+    image_ptr_->rgb_image_ = img_ptr->image;
+    image_ptr_->depth_image_ = depth_img_ptr->image;
+    image_ptr_->frame_id_ = rgb_image_msg->header.frame_id;
+    image_ptr_->timestamp_ = rgb_image_msg->header.stamp.toSec();
+}
+
+// ----------------------------------------------------------------------------------------
+
+void Client::camInfoCallback(const sensor_msgs::CameraInfoConstPtr& cam_info_msg)
+{
+    if (!ros_image_sync_data_->cam_model_.initialized())
+        ros_image_sync_data_->cam_model_.fromCameraInfo(cam_info_msg);
+}
+
+// ----------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------
+
+void Client::rgbdImageCallback(const rgbd::RGBDMsg::ConstPtr& msg) {
 
     if (msg->version == 0)
     {
