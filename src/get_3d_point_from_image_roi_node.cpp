7955492f83e068b3872a896cc17bb0722ce3d5b5
Rein Appeldoorn <reinzor@gmail.com>
2017-05-06 19:08:12 +0200

    API change, now a timestamp and multiple rois can be specified

--- src/get_3d_point_from_image_roi_node.cpp
+++ src/get_3d_point_from_image_roi_node.cpp
@@ -18,63 +18,90 @@
 #include <memory>
 
 #include <rgbd/Project2DTo3D.h>
+#include <boost/circular_buffer.hpp>
 
 // ----------------------------------------------------------------------------------------------------
 
-std::shared_ptr<rgbd::Image> g_last_img;
+boost::circular_buffer<std::shared_ptr<rgbd::Image>> g_last_images_;
 bool srvGet3dPointFromRoi(rgbd::Project2DTo3D::Request& req, rgbd::Project2DTo3D::Response& res)
 {
-  if (!g_last_img) {
-    ROS_ERROR("I did not receive a rgbd image yet ...");
-    return false;
-  }
-  const sensor_msgs::RegionOfInterest& roi = req.roi;
+  std::shared_ptr<rgbd::Image> last_image;
 
-  const cv::Mat& depth = g_last_img->getDepthImage();
-
-  cv::Rect roi_rgb(roi.x_offset, roi.y_offset, roi.width, roi.height);
-  float rgb_depth_width_ratio = (float)(depth.cols) / g_last_img->getRGBImage().cols;
-  cv::Rect roi_depth(rgb_depth_width_ratio * roi_rgb.tl(), rgb_depth_width_ratio * roi_rgb.br());
-  cv::Point roi_depth_center = 0.5 * (roi_depth.tl() + roi_depth.br());
-
-  cv::Rect roi_depth_capped(cv::Point(std::max(0, roi_depth.x),
-                                      std::max(0, roi_depth.y)),
-                            cv::Point(std::min(depth.cols - 1, roi_depth.br().x),
-                                      std::min(depth.rows - 1, roi_depth.br().y)));
-
-  cv::Mat depth_roi_capped = depth(roi_depth_capped);
-
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  // Get median depth of ROI
-
-  std::vector<float> depths;
-  for(unsigned int j = 0; j < depth_roi_capped.cols * depth_roi_capped.rows; ++j)
+  if (!g_last_images_.empty())
   {
-      float d = depth_roi_capped.at<float>(j);
-      if (d > 0 && d == d)
-          depths.push_back(d);
+    if (req.stamp == ros::Time(0))
+    {
+      last_image = g_last_images_.back();
+    }
+    else
+    {
+      for (auto it = g_last_images_.rbegin(); it != g_last_images_.rend(); ++it)
+      {
+        if ( (*it)->getTimestamp() <= req.stamp.toSec() )
+        {
+          last_image = *it;
+        }
+      }
+    }
   }
 
-  if (depths.empty())
-  {
-    ROS_ERROR("All depths within ROI are invalid!");
+  if (!last_image) {
+    ROS_ERROR("I could not find an images in my image buffer for timestamp %.2f", req.stamp.toSec());
     return false;
   }
 
-  std::sort(depths.begin(), depths.end());
-  float median_depth = depths[depths.size() / 2];
+  for (const sensor_msgs::RegionOfInterest& roi : req.rois)
+  {
+    const cv::Mat& depth = last_image->getDepthImage();
 
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  // Determine roi location
+    cv::Rect roi_rgb(roi.x_offset, roi.y_offset, roi.width, roi.height);
+    float rgb_depth_width_ratio = (float)(depth.cols) / last_image->getRGBImage().cols;
+    cv::Rect roi_depth(rgb_depth_width_ratio * roi_rgb.tl(), rgb_depth_width_ratio * roi_rgb.br());
+    cv::Point roi_depth_center = 0.5 * (roi_depth.tl() + roi_depth.br());
 
-  rgbd::View view(*g_last_img, g_last_img->getDepthImage().cols);
-  geo::Vec3 pos = view.getRasterizer().project2Dto3D(roi_depth_center.x, roi_depth_center.y) * median_depth;
+    cv::Rect roi_depth_capped(cv::Point(std::max(0, roi_depth.x),
+                                        std::max(0, roi_depth.y)),
+                              cv::Point(std::min(depth.cols - 1, roi_depth.br().x),
+                                        std::min(depth.rows - 1, roi_depth.br().y)));
 
-  ROS_INFO("Pose (%.2f, %.2f, %.2f)", pos.x, pos.y, pos.z);
+    cv::Mat depth_roi_capped = depth(roi_depth_capped);
 
-  res.point.header.frame_id = g_last_img->getFrameId();
-  res.point.header.stamp = ros::Time(g_last_img->getTimestamp());
-  geo::convert(pos, res.point.point);
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Get median depth of ROI
+
+    std::vector<float> depths;
+    for(unsigned int j = 0; j < depth_roi_capped.cols * depth_roi_capped.rows; ++j)
+    {
+        float d = depth_roi_capped.at<float>(j);
+        if (d > 0 && d == d)
+            depths.push_back(d);
+    }
+
+    if (depths.empty())
+    {
+      ROS_ERROR("All depths within ROI are invalid!");
+      return false;
+    }
+
+    std::sort(depths.begin(), depths.end());
+    float median_depth = depths[depths.size() / 2];
+
+    // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    // Determine roi location
+
+    rgbd::View view(*last_image, last_image->getDepthImage().cols);
+    geo::Vec3 pos = view.getRasterizer().project2Dto3D(roi_depth_center.x, roi_depth_center.y) * median_depth;
+
+    ROS_INFO("Pose (%.2f, %.2f, %.2f)", pos.x, pos.y, pos.z);
+
+    geometry_msgs::PointStamped point_msg;
+    point_msg.header.frame_id = last_image->getFrameId();
+    point_msg.header.stamp = ros::Time(last_image->getTimestamp());
+    geo::convert(pos, point_msg.point);
+
+    res.points.push_back(point_msg);
+  }
+
   return true;
 }
 
@@ -87,6 +114,8 @@ int main(int argc, char **argv)
   // Listener
   rgbd::Client client;
   client.intialize("rgbd");
+
+  g_last_images_.set_capacity(100);
 
   // srv
   ros::NodeHandle nh;
@@ -101,7 +130,7 @@ int main(int argc, char **argv)
     {
       if (image.getDepthImage().data)
       {
-        g_last_img = std::shared_ptr<rgbd::Image>(new rgbd::Image(image));
+        g_last_images_.push_back(std::shared_ptr<rgbd::Image>(new rgbd::Image(image)));
         ROS_DEBUG("Got msg...");
       }
     }
