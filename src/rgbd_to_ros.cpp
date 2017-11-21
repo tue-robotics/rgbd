1fc339ca1d62833d6c39a0ecc4a606410efa2408
Matthijs van der Burgh <matthijs.vander.burgh@live.nl>
2017-11-21 15:06:40 +0100

    seperate point_cloud publisher
    remove unused dependencies

--- src/rgbd_to_ros.cpp
+++ src/rgbd_to_ros.cpp
@@ -7,11 +7,6 @@
 
 #include <rgbd/ros/conversions.h>
 
-// Point cloud
-#include <pcl_ros/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl/point_cloud.h>
-
 int main(int argc, char **argv)
 {
     if (argc <= 1)
@@ -32,7 +27,6 @@ int main(int argc, char **argv)
     ros::Publisher pub_depth_info = nh.advertise<sensor_msgs::CameraInfo>("depth/camera_info", 1);
     ros::Publisher pub_rgb_img = nh.advertise<sensor_msgs::Image>("rgb/image", 1);
     ros::Publisher pub_depth_img = nh.advertise<sensor_msgs::Image>("depth/image", 1);
-    ros::Publisher pub_depth_pc = nh.advertise<pcl::PointCloud<pcl::PointXYZ> >("depth/points", 1);
 
     ros::Time last_image_stamp;
 
@@ -68,45 +62,6 @@ int main(int argc, char **argv)
                 // Publish
                 pub_depth_img.publish(msg);
                 pub_depth_info.publish(info_msg);
-
-//                 // Create point cloud
-//                 pcl::PointCloud<pcl::PointXYZRGB>::Ptr pc_msg(new pcl::PointCloud<pcl::PointXYZRGB>());
-
-//                 pc_msg->header.stamp = image.getTimestamp() * 1e6;
-//                 pc_msg->header.frame_id = image.getFrameId();
-//                 pc_msg->width  = 0;
-//                 pc_msg->height  = 1;
-//                 pc_msg->is_dense = true;
-
-//                 // Fill point cloud
-//                 for(int y = 0; y < view.getHeight(); ++y)
-//                 {
-//                     for(int x = 0; x < view.getWidth(); ++x)
-//                     {
-//                         geo::Vector3 p;
-//                         if (view.getPoint3D(x, y, p))
-//                         {
-//                             const cv::Vec3b& c = view.getColor(x, y);
-
-//                             // Push back and correct for geolib frame
-//                             pc_msg->points.push_back(pcl::PointXYZRGB());
-//                             pcl::PointXYZRGB& p_pcl = pc_msg->points.back();
-//                             p_pcl.x = p.x; p_pcl.y = -p.y; p_pcl.z = -p.z;
-//                             p_pcl.r = c[2]; p_pcl.g = c[1]; p_pcl.b = c[0];
-//                             pc_msg->width++;
-// //                            std::cout << p << std::endl;
-//                         }
-//                         else
-//                         {
-// //                            pc_msg->points.push_back(pcl::PointXYZ(0, 0, 0));
-// //                            pc_msg->width++;
-//                             pc_msg->is_dense = false;
-//                         }
-//                     }
-//                 }
-
-//                 // Publish
-//                 pub_depth_pc.publish(pc_msg);
             }
 
             if (image.getRGBImage().data)
