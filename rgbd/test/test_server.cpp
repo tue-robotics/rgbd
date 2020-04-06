dc613bab6392de161b1240db1f96f7e1e24bb8f2
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-04-06 23:40:44 +0200

    Fix memory leak in test_server

--- rgbd/test/test_server.cpp
+++ rgbd/test/test_server.cpp
@@ -3,6 +3,8 @@
 #include <ros/init.h>
 #include <ros/rate.h>
 
+#include <image_geometry/pinhole_camera_model.h>
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "rgbd_transport_test_server");
@@ -11,27 +13,31 @@ int main(int argc, char **argv)
     server.initialize("test", rgbd::RGB_STORAGE_LOSSLESS, rgbd::DEPTH_STORAGE_LOSSLESS);
 
     ros::Rate r(30);
+    cv::Mat rgb_image(480, 640, CV_8UC3, cv::Scalar(0,0,255));
+    cv::Mat depth_image(480, 640, CV_32FC1, 5.0);
+    image_geometry::PinholeCameraModel cam_model;
+
+    rgbd::Image image(rgb_image, depth_image, cam_model, "test_frame_id", ros::Time::now().toSec());
 
     int x = 0;
     while (ros::ok())
     {
-        cv::Mat rgb_image(480, 640, CV_8UC3, cv::Scalar(0,0,255));
+        cv::line(rgb_image, cv::Point(x, 0), cv::Point(x, rgb_image.rows - 1), cv::Scalar(0,0,255));
+        cv::line(depth_image, cv::Point(x, 0), cv::Point(x, depth_image.rows - 1), 5.0);
+        x = (x + 10) % rgb_image.cols;
         cv::line(rgb_image, cv::Point(x, 0), cv::Point(x, rgb_image.rows - 1), cv::Scalar(255, 0, 0));
-
-        cv::Mat depth_image(480, 640, CV_32FC1, 5.0);
         cv::line(depth_image, cv::Point(x, 0), cv::Point(x, depth_image.rows - 1), 1.0);
 
-        geo::DepthCamera cam_model;
-        rgbd::Image image(rgb_image, depth_image, cam_model, "test_frame_id", ros::Time::now().toSec());
+        image.setRGBImage(rgb_image);
+        image.setDepthImage(depth_image);
+        image.setTimestamp(ros::Time::now().toSec());
+
+        rgbd::Image image2(rgb_image, depth_image, cam_model, "test_frame_id", ros::Time::now().toSec());
 
         server.send(image, true);
 
-        x = (x + 10) % rgb_image.cols;
-
         r.sleep();
     }
-
-    ros::spin();
 
     return 0;
 }
