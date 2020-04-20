ddd9805a29f181852baf6eecba3b8814746a87f2
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-04-20 23:46:00 +0200

    (Server) docstrings and cleanup

--- rgbd/include/rgbd/Server.h
+++ rgbd/include/rgbd/Server.h
@@ -1,51 +1,54 @@
-#ifndef RGBD_TRANSPORT_SERVER_H_
-#define RGBD_TRANSPORT_SERVER_H_
+#ifndef RGBD_SERVER_H_
+#define RGBD_SERVER_H_
 
 #include "rgbd/Image.h"
 #include "rgbd/Server_RGBD.h"
 #include "rgbd/Server_SHM.h"
 
-#include "rgbd_msgs/GetRGBD.h"
-
-#include <ros/publisher.h>
-#include <ros/callback_queue.h>
-#include <ros/service_server.h>
-
-#include <boost/thread.hpp>
-
 namespace rgbd {
 
 class Image;
 
+/**
+ * @brief Server which provides interfaces of ServerRGBD and ServerSHM
+ */
 class Server {
 
 public:
 
+    /**
+     * @brief Constructor
+     */
     Server();
 
+    /**
+     * @brief Destructor
+     */
     virtual ~Server();
 
     /**
      * @brief initialize initialize server
      * @param name Fully resolved server name
-     * @param rgb_type
-     * @param depth_type
+     * @param rgb_type rgb storage type
+     * @param depth_type depth storage type
      */
     void initialize(const std::string& name, RGBStorageType rgb_type = RGB_STORAGE_LOSSLESS, DepthStorageType depth_type = DEPTH_STORAGE_LOSSLESS);
 
+    /**
+     * @brief send Write a new image to all interfaces
+     * @param image Image to be written
+     * @param threaded Use any threaded version of send of the interfaces
+     */
     void send(const Image& image, bool threaded=false);
 
 protected:
-
-    rgbd::Image image_;
 
     ServerRGBD server_rgbd_;
 
     ServerSHM server_shm_;
 
-
 };
 
 }
 
-#endif
+#endif // RGBD_SERVER_H_
