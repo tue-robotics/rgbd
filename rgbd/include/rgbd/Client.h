7e4e37ec2e525ca1a6534b7ab0e45965beca644a
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-04-20 23:50:28 +0200

    (Client) add docstrings

--- rgbd/include/rgbd/Client.h
+++ rgbd/include/rgbd/Client.h
@@ -18,12 +18,21 @@
 
 namespace rgbd {
 
+/**
+ * @brief Client which provides the interfaces of ClientRGBD and ClientSHM
+ */
 class Client {
 
 public:
 
+    /**
+     * @brief Constructor
+     */
     Client();
 
+    /**
+     * @brief Destructor
+     */
     virtual ~Client();
 
     /**
@@ -32,10 +41,26 @@ public:
      */
     void intialize(const std::string& server_name, float timeout = 5.0);
 
+    /**
+     * @brief Check if the client is initialized. First checks if ClientSHM is initialized, otherwise ClientRGBD.
+     * nextImage will not return an image if client is not initialized.
+     * @return initialized or not
+     */
     bool initialized() { return (client_shm_.initialized() || client_rgbd_.initialized()); }
 
+    /**
+     * @brief Get a new Image. If no new image has been received since the last call,
+     * no image will be written and false will be returned.
+     * @param image Image reference which will be written.
+     * @return valid image written
+     */
     bool nextImage(Image& image);
 
+    /**
+     * @brief Get a new Image. If no new image has been received since the last call,
+     * The ImagePtr will be a nullptr
+     * @return ImagePtr to an Image or a nullptr
+     */
     ImagePtr nextImage();
 
 protected:
