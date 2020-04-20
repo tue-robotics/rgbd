8693dc0bcd897edb2b3141bb6e70b218c1d68074
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-04-20 23:43:26 +0200

    (ServerRGBD) add docstrings

--- rgbd/include/rgbd/Server_RGBD.h
+++ rgbd/include/rgbd/Server_RGBD.h
@@ -1,5 +1,5 @@
-#ifndef SERVER_RGBD_H
-#define SERVER_RGBD_H
+#ifndef RGBD_SERVER_RGBD_H
+#define RGBD_SERVER_RGBD_H
 
 #include "rgbd/Image.h"
 #include "rgbd_msgs/GetRGBD.h"
@@ -13,24 +13,43 @@
 
 namespace rgbd {
 
+/**
+ * @brief Server which provides RGBD topic and RGBD service
+ */
 class ServerRGBD {
 
 public:
 
+    /**
+     * @brief Constructor
+     */
     ServerRGBD();
 
+    /**
+     * @brief Destructor
+     *
+     * Nodehandle is shutdown and all threads are joined
+     */
     virtual ~ServerRGBD();
 
     /**
      * @brief initialize initialize server
      * @param name Fully resolved server name
-     * @param rgb_type
-     * @param depth_type
+     * @param rgb_type rgb storage type
+     * @param depth_type depth storage type
      */
     void initialize(const std::string& name, RGBStorageType rgb_type = RGB_STORAGE_LOSSLESS, DepthStorageType depth_type = DEPTH_STORAGE_LOSSLESS, const float service_freq = 10);
 
+    /**
+     * @brief send Write a new image to all interfaces
+     * @param image Image to be written
+     * @param threaded Use any threaded version of send of the interfaces
+     */
     void send(const Image& image, bool threaded = false);
 
+    /**
+     * @brief MESSAGE_VERSION version of the RGBD message being used
+     */
     const static int MESSAGE_VERSION;
 
 protected:
@@ -52,14 +71,28 @@ protected:
     // Send thread
     boost::thread send_thread_;
 
+    /**
+     * @brief sendImpl Implementaiton of send
+     * @param image Image to be written
+     */
     void sendImpl(const Image& image);
 
+    /**
+     * @brief serviceCallback
+     * @param req Service Request
+     * @param resp Service Response
+     * @return succes
+     */
     bool serviceCallback(rgbd_msgs::GetRGBDRequest& req, rgbd_msgs::GetRGBDResponse& resp);
 
+    /**
+     * @brief Function to be called in the thread proving the service
+     * @param frequency frequency for checking service requests
+     */
     void serviceThreadFunc(const float frequency);
 
 };
 
 }
 
-#endif // SERVER_RGBD_H
+#endif // RGBD_SERVER_RGBD_H
