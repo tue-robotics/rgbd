542d94dce968c6abe0db804ea297df2562377722
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-04-20 22:44:58 +0200

    (Server) refactor to composition

--- rgbd/include/rgbd/Server.h
+++ rgbd/include/rgbd/Server.h
@@ -2,6 +2,7 @@
 #define RGBD_TRANSPORT_SERVER_H_
 
 #include "rgbd/Image.h"
+#include "rgbd/Server_RGBD.h"
 #include "rgbd/Server_SHM.h"
 
 #include "rgbd_msgs/GetRGBD.h"
@@ -32,31 +33,16 @@ public:
      */
     void initialize(const std::string& name, RGBStorageType rgb_type = RGB_STORAGE_LOSSLESS, DepthStorageType depth_type = DEPTH_STORAGE_LOSSLESS);
 
-    void send(const Image& image, bool threaded = false);
-
-    const static int SERIALIZATION_VERSION;
+    void send(const Image& image, bool threaded=false);
 
 protected:
 
-    ros::Publisher pub_image_;
-    ros::ServiceServer service_server_;
-    ros::CallbackQueue cb_queue_;
+    rgbd::Image image_;
 
-    RGBStorageType rgb_type_;
-    DepthStorageType depth_type_;
-
-    rgbd::Image image_copy_;
+    ServerRGBD server_rgbd_;
 
     ServerSHM server_shm_;
 
-    // Threaded sending
-    boost::mutex send_mutex_shared_;
-    boost::mutex send_mutex_topic_;
-    boost::thread send_thread_;
-
-    void sendImpl(const Image& image);
-
-    bool serviceServerCallback(rgbd_msgs::GetRGBDRequest& req, rgbd_msgs::GetRGBDResponse& resp);
 
 };
 
--- rgbd/src/Server.cpp
+++ rgbd/src/Server.cpp
@@ -1,17 +1,6 @@
 #include "rgbd/Server.h"
-#include "rgbd_msgs/RGBD.h"
-#include "rgbd/Image.h"
-#include "rgbd/serialization.h"
-
-#include <opencv2/highgui/highgui.hpp>
-
-#include <tue/serialization/conversions.h>
-
-#include <ros/node_handle.h>
 
 namespace rgbd {
-
-const int Server::SERIALIZATION_VERSION = 2;
 
 // ----------------------------------------------------------------------------------------
 
@@ -23,22 +12,13 @@ Server::Server()
 
 Server::~Server()
 {
-    send_thread_.join();
 }
 
 // ----------------------------------------------------------------------------------------
 
 void Server::initialize(const std::string& name, RGBStorageType rgb_type, DepthStorageType depth_type)
 {
-    ros::NodeHandle nh;
-    pub_image_ = nh.advertise<rgbd_msgs::RGBD>(name, 1);
-    rgb_type_ = rgb_type;
-    depth_type_ = depth_type;
-
-    nh.setCallbackQueue(&cb_queue_);
-    service_server_ = nh.advertiseService(name, &Server::serviceServerCallback, this);
-
-    // Initialize shared mem server
+    server_rgbd_.initialize(name, rgb_type, depth_type);
     server_shm_.initialize(name);
 }
 
@@ -46,80 +26,9 @@ void Server::initialize(const std::string& name, RGBStorageType rgb_type, DepthS
 
 void Server::send(const Image& image, bool threaded)
 {
-    threaded = false; // Running threaded causes a memory leak.
-    if (!threaded)
-    {
-        sendImpl(image);
-        return;
-    }
+    server_rgbd_.send(image, threaded);
+    server_shm_.send(image);
 
-    send_thread_ = boost::thread(&Server::sendImpl, this, image);
-}
-
-// ----------------------------------------------------------------------------------------
-
-void Server::sendImpl(const Image& image)
-{    
-    image_copy_ = image.clone();
-
-    // Handle the service
-    cb_queue_.callAvailable();
-
-    // Send image using shared memory
-    {
-        boost::mutex::scoped_lock lock(send_mutex_shared_, boost::try_to_lock);
-        if (!lock)
-            return;
-
-        server_shm_.send(image_copy_);
-    }
-
-    if (pub_image_.getNumSubscribers() == 0)
-        return;
-
-    rgbd_msgs::RGBD msg;
-    msg.version = SERIALIZATION_VERSION;
-
-    std::stringstream stream;
-    tue::serialization::OutputArchive a(stream);
-    serialize(image_copy_, a, rgb_type_, depth_type_);
-    tue::serialization::convert(stream, msg.rgb);
-
-    {
-        boost::mutex::scoped_lock lock(send_mutex_topic_, boost::try_to_lock);
-        if (!lock)
-            return;
-
-        pub_image_.publish(msg);
-    }
-}
-
-bool Server::serviceServerCallback(rgbd_msgs::GetRGBDRequest& req, rgbd_msgs::GetRGBDResponse& resp)
-{
-    //! Check for valid input
-    if (req.compression != rgbd_msgs::GetRGBDRequest::JPEG && req.compression != rgbd_msgs::GetRGBDRequest::PNG)
-    {
-        ROS_ERROR("Invalid compression, only JPEG and PNG are supported (see ENUM in srv definition)");
-        return false;
-    }
-
-    //! Create resized images
-    cv::Mat resized_rgb, resized_depth;
-
-    double ratio_rgb = (double) req.width / image_copy_.getRGBImage().cols;
-    double ratio_depth = (double) req.width / image_copy_.getDepthImage().cols;
-
-    cv::resize(image_copy_.getRGBImage(), resized_rgb, cv::Size(req.width, image_copy_.getRGBImage().rows * ratio_rgb));
-    cv::resize(image_copy_.getDepthImage(), resized_depth, cv::Size(req.width, image_copy_.getDepthImage().rows * ratio_depth));
-
-    // Compress images
-    std::string compression_str = req.compression == rgbd_msgs::GetRGBDRequest::JPEG ? ".jpeg" : ".png";
-    if (cv::imencode(compression_str, resized_rgb, resp.rgb_data) && cv::imencode(compression_str, resized_depth, resp.depth_data))
-        return true;
-
-    ROS_ERROR_STREAM("cv::imencode with compression_str " << compression_str << " failed!");
-
-    return false;
 }
 
 }
