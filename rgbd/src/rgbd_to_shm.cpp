ed8757a0ce26b10996b43ba178ec40e42e0ec53d
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-06-01 12:49:31 +0200

    (rgbd_to_shm) publish hostname

--- rgbd/src/rgbd_to_shm.cpp
+++ rgbd/src/rgbd_to_shm.cpp
@@ -3,16 +3,38 @@
 #include <ros/master.h>
 #include <ros/names.h>
 #include <ros/node_handle.h>
+#include <ros/publisher.h>
 #include <ros/rate.h>
 
 #include "rgbd/client_rgbd.h"
 #include "rgbd/image.h"
 #include "rgbd/server_shm.h"
+#include "rgbd/tools.h"
 #include "rgbd/types.h"
+
+#include <std_msgs/String.h>
+
+#include <thread>
 
 // ----------------------------------------------------------------------------------------
 
-int main(int argc, char **argv) {
+void pubHostnameThreadFunc(ros::NodeHandle& nh, const std::string server_name, const std::string hostname, const float frequency)
+{
+    ros::Publisher pub_shm_hostname = nh.advertise<std_msgs::String>(server_name + "/hosts", 1);
+    ros::Rate r(frequency);
+    std_msgs::String msg;
+    msg.data = hostname;
+    while(nh.ok())
+    {
+        pub_shm_hostname.publish(msg);
+        r.sleep();
+    }
+}
+
+// ----------------------------------------------------------------------------------------
+
+int main(int argc, char **argv)
+{
     ros::init(argc, argv, "rgbd_to_shm");
 
     ros::NodeHandle nh_private("~");
@@ -26,9 +48,13 @@ int main(int argc, char **argv) {
     rgbd::ServerSHM server;
 
     const std::string server_name = ros::names::resolve("rgbd");
+    const std::string host_name = rgbd::get_hostname();
 
     client.intialize(server_name);
     server.initialize(server_name);
+
+    ros::NodeHandle nh;
+    std::thread pub_hostname_thread(pubHostnameThreadFunc, std::ref(nh), server_name, host_name, 10);
 
     rgbd::ImagePtr image_ptr;
 
@@ -45,6 +71,9 @@ int main(int argc, char **argv) {
             server.send(*image_ptr);
         r.sleep();
     }
+
+    nh.shutdown();
+    pub_hostname_thread.join();
 
     return 0;
 }
