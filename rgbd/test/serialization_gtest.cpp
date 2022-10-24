6b8a34f0bf6edc017e2d1b57178511be8057be00
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-10-24 11:06:55 +0200

    (test) towards serialization test

--- rgbd/test/serialization_gtest.cpp
+++ rgbd/test/serialization_gtest.cpp
@@ -1,14 +1,73 @@
 #include <gtest/gtest.h>
 
+#include <image_geometry/pinhole_camera_model.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/distortion_models.h>
+
+#include <rgbd/image.h>
+#include <rgbd/serialization.h>
+
+#include <ros/console.h>
+
+#include <tue/serialization/input_archive.h>
+#include <tue/serialization/output_archive.h>
+
+#include <sstream>
+
 TEST(serialization, True)
 {
-    EXPECT_TRUE(true);
+    cv::Mat rgb_image(480, 640, CV_8UC3, cv::Scalar(0,0,255));
+    cv::Mat depth_image(480, 640, CV_32FC1, 5.0);
+    sensor_msgs::CameraInfo cam_info;
+    cam_info.D.resize(5, 0.0);
+    cam_info.K = {554.2559327880068, 0.0, 320.5,
+                  0.0, 554.2559327880068, 240.5,
+                  0.0, 0.0, 1.0};
+    cam_info.R.fill(0.0);
+    cam_info.R[0] = 1.0;
+    cam_info.R[4] = 1.0;
+    cam_info.R[8] = 1.0;
+    cam_info.P = {554.2559327880068, 0.0, 320.5, 0.0,
+                  0.0, 554.2559327880068, 240.5, 0.0,
+                  0.0, 0.0, 1.0, 0.0};
+    cam_info.distortion_model = sensor_msgs::distortion_models::PLUMB_BOB;
+    cam_info.width = 640;
+    cam_info.height = 480;
+    image_geometry::PinholeCameraModel cam_model;
+    cam_model.fromCameraInfo(cam_info);
+
+    rgbd::Image image(rgb_image, depth_image, cam_model, "test_frame_id", 5);
+    rgbd::Image image2;
+
+    std::stringstream ss;
+    tue::serialization::OutputArchive output_achive(ss);
+
+    EXPECT_TRUE(rgbd::serialize(image, output_achive, rgbd::RGB_STORAGE_JPG));
+    tue::serialization::InputArchive input_achive(ss);
+    EXPECT_TRUE(rgbd::deserialize(input_achive, image2));
+
+    cv::Mat dst;
+    const cv::Mat& image_depth = image.getDepthImage();
+    const cv::Mat& image2_depth = image2.getDepthImage();
+    cv::bitwise_xor(image_depth, image2_depth, dst);
+    EXPECT_TRUE(cv::countNonZero(dst) == 0);
+
+//    const cv::Mat& image_rgb = image.getRGBImage();
+//    const cv::Mat& image2_rgb = image2.getRGBImage();
+//    cv::bitwise_xor(image_rgb, image2_rgb, dst);
+//    cv::Mat dst2;
+//    cv::threshold(dst, dst2, 1, 1, cv::THRESH_BINARY);
+//    EXPECT_TRUE(cv::countNonZero(dst2) == 0);
+
+    EXPECT_TRUE(image.getCameraModel().cameraInfo() == image2.getCameraModel().cameraInfo());
+    EXPECT_TRUE(image.getFrameId() == image2.getFrameId());
+    EXPECT_TRUE(image.getTimestamp() == image2.getTimestamp());
 }
 
-TEST(deserialization, True)
-{
-    EXPECT_TRUE(true);
-}
+//TEST(deserialization, True)
+//{
+//    EXPECT_TRUE(true);
+//}
 
 
 // Run all the tests that were declared with TEST()
