bd1b77f7d0feafafdfa79ea5c29f7723825a98a0
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-10-26 13:29:46 +0200

    (test) get rgb serialization working

--- rgbd/test/serialization_gtest.cpp
+++ rgbd/test/serialization_gtest.cpp
@@ -41,7 +41,7 @@ TEST(serialization, True)
     std::stringstream ss;
     tue::serialization::OutputArchive output_achive(ss);
 
-    EXPECT_TRUE(rgbd::serialize(image, output_achive, rgbd::RGB_STORAGE_JPG));
+    EXPECT_TRUE(rgbd::serialize(image, output_achive, rgbd::RGB_STORAGE_LOSSLESS));
     tue::serialization::InputArchive input_achive(ss);
     EXPECT_TRUE(rgbd::deserialize(input_achive, image2));
 
@@ -51,12 +51,12 @@ TEST(serialization, True)
     cv::bitwise_xor(image_depth, image2_depth, dst);
     EXPECT_TRUE(cv::countNonZero(dst) == 0);
 
-//    const cv::Mat& image_rgb = image.getRGBImage();
-//    const cv::Mat& image2_rgb = image2.getRGBImage();
-//    cv::bitwise_xor(image_rgb, image2_rgb, dst);
-//    cv::Mat dst2;
-//    cv::threshold(dst, dst2, 1, 1, cv::THRESH_BINARY);
-//    EXPECT_TRUE(cv::countNonZero(dst2) == 0);
+    const cv::Mat& image_rgb = image.getRGBImage();
+    const cv::Mat& image2_rgb = image2.getRGBImage();
+    cv::bitwise_xor(image_rgb, image2_rgb, dst);
+    cv::Mat dst2;
+    cv::transform(dst, dst2, cv::Matx<int, 1, 3>(1,1,1));
+    EXPECT_TRUE(cv::countNonZero(dst2) == 0);
 
     EXPECT_TRUE(image.getCameraModel().cameraInfo() == image2.getCameraModel().cameraInfo());
     EXPECT_TRUE(image.getFrameId() == image2.getFrameId());
