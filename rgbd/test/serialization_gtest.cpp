2274da88c1e4610276696023882dd0f44a01baab
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2022-12-14 10:03:23 +0100

    (test) use image equality operators

--- rgbd/test/serialization_gtest.cpp
+++ rgbd/test/serialization_gtest.cpp
@@ -22,26 +22,6 @@ protected:
         image1 = rgbd::generateRandomImage();
     }
 
-    static void compareImages(const rgbd::Image& image1, const rgbd::Image& image2)
-    {
-        const cv::Mat& image1_depth = image1.getDepthImage();
-        const cv::Mat& image2_depth = image2.getDepthImage();
-        cv::Mat dst;
-        cv::bitwise_xor(image1_depth, image2_depth, dst);
-        EXPECT_TRUE(cv::countNonZero(dst) == 0);
-
-        const cv::Mat& image1_rgb = image1.getRGBImage();
-        const cv::Mat& image2_rgb = image2.getRGBImage();
-        cv::bitwise_xor(image1_rgb, image2_rgb, dst);
-        cv::Mat dst2;
-        cv::transform(dst, dst2, cv::Matx<int, 1, 3>(1,1,1));
-        EXPECT_TRUE(cv::countNonZero(dst2) == 0);
-
-        EXPECT_TRUE(image1.getCameraModel().cameraInfo() == image2.getCameraModel().cameraInfo());
-        EXPECT_TRUE(image1.getFrameId() == image2.getFrameId());
-        EXPECT_TRUE(image1.getTimestamp() == image2.getTimestamp());
-    }
-
     rgbd::Image image1;
     rgbd::Image image2;
 };
@@ -55,7 +35,7 @@ TEST_F(Serialization, LossLess)
     tue::serialization::InputArchive input_achive(ss);
     EXPECT_TRUE(rgbd::deserialize(input_achive, image2));
 
-    compareImages(image1, image2);
+    EXPECT_EQ(image1, image2);
 }
 
 TEST_F(Serialization, Lossy)
@@ -78,7 +58,7 @@ TEST_F(Serialization, Lossy)
     cv::imencode(".jpg", image1.getRGBImage(), rgb_data, rgb_params);
     image1.setRGBImage(cv::imdecode(rgb_data, cv::IMREAD_UNCHANGED));
 
-    compareImages(image1, image2);
+    EXPECT_EQ(image1, image2);
 }
 
 // Run all the tests that were declared with TEST()
