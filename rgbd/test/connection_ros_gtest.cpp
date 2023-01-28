181f219f6a714df4cd624251387503bcfa019765
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2023-01-28 11:31:00 +0100

    (Test) extend connection ROS test

--- rgbd/test/connection_ros_gtest.cpp
+++ rgbd/test/connection_ros_gtest.cpp
@@ -118,6 +118,82 @@ TEST_F(ROS, NextImageTwice)
     EXPECT_FALSE(ros::isShuttingDown());
 }
 
+TEST_F(ROS, NextImageTwiceWithoutSend)
+{
+    EXPECT_FALSE(ros::isShuttingDown());
+    EXPECT_TRUE(client.initialize("rgb/image", "depth/image", "rgb/camera_info"));
+    EXPECT_TRUE(client.initialized());
+    rgbd::Image image2;
+    for (uint i=0; i<2; ++i)	
+    {	
+        client.nextImage(image2);	
+        server.send(image);	
+        ros::Duration(0.05).sleep();	
+    }
+    EXPECT_TRUE(client.nextImage(image2));
+    EXPECT_EQ(image, image2);
+    EXPECT_FALSE(ros::isShuttingDown());
+    EXPECT_FALSE(client.nextImage(image2));
+    EXPECT_FALSE(ros::isShuttingDown());
+}
+
+TEST_F(ROS, NextImagePtrTwice)
+{
+    EXPECT_FALSE(ros::isShuttingDown());
+    EXPECT_TRUE(client.initialize("rgb/image", "depth/image", "rgb/camera_info"));
+    EXPECT_TRUE(client.initialized());
+    server.send(image);
+    rgbd::ImagePtr image2;
+    for (uint i=0; i<2; ++i)	
+    {	
+        image2 = client.nextImage();	
+        server.send(image);	
+        ros::Duration(0.05).sleep();	
+    }
+    EXPECT_TRUE(image2);
+    if (image2) // This prevents a crash of the node. Test will still fail because of previous line
+    {
+        EXPECT_EQ(image, *image2);
+    }
+    EXPECT_FALSE(ros::isShuttingDown());
+    image.setTimestamp(image.getTimestamp()+10.);
+    image2.reset();
+    server.send(image);
+    ros::Duration(0.05).sleep();
+    image2 = client.nextImage();
+    EXPECT_TRUE(image2);
+    if (image2) // This prevents a crash of the node. Test will still fail because of previous line
+    {
+        EXPECT_EQ(image, *image2);
+    }
+    EXPECT_FALSE(ros::isShuttingDown());
+}
+
+TEST_F(ROS, NextImagePtrTwiceWithoutSend)
+{
+    EXPECT_FALSE(ros::isShuttingDown());
+    EXPECT_TRUE(client.initialize("rgb/image", "depth/image", "rgb/camera_info"));
+    EXPECT_TRUE(client.initialized());
+    server.send(image);
+    rgbd::ImagePtr image2;
+    for (uint i=0; i<2; ++i)	
+    {	
+        image2 = client.nextImage();	
+        server.send(image);	
+        ros::Duration(0.05).sleep();	
+    }
+    EXPECT_TRUE(image2);
+    if (image2) // This prevents a crash of the node. Test will still fail because of previous line
+    {
+        EXPECT_EQ(image, *image2);
+    }
+    EXPECT_FALSE(ros::isShuttingDown());
+    image2.reset();
+    image2 = client.nextImage();
+    EXPECT_FALSE(image2);
+    EXPECT_FALSE(ros::isShuttingDown());
+}
+
 TEST_F(ROS_NS, NextImage)
 {
     EXPECT_FALSE(ros::isShuttingDown());
