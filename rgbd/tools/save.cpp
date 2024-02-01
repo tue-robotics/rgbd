3a8a846b7f32e403b121eec1229f18a4a32155d9
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2024-02-01 12:56:09 +0100

    (save) fix suppression of warnings

--- rgbd/tools/save.cpp
+++ rgbd/tools/save.cpp
@@ -47,11 +47,13 @@ int main(int argc, char **argv)
             }
         }
         ROS_INFO("Press s to save and q to exit.");
-        
+
+        #pragma GCC diagnostic push
         #pragma GCC diagnostic ignored "-Wunused-result"
         system ("/bin/stty raw");
         key_pressed = getchar();
         system ("/bin/stty cooked");
+        #pragma GCC diagnostic pop
         
         if (key_pressed == 's')
         {
