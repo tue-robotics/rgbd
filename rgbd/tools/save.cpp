4dfe7e97db4a8dc47a0d065549e4e3bc4db7beee
Gustavo David Cordova Cardenas <99350669+GustavoDCC@users.noreply.github.com>
2024-02-27 19:46:12 +0100

    detect key pressed to save rgbd (#78)
    * detect key pressed to save rgbd
    
    * edit key pressed detection
    
    * file overwrite checking
    
    * add time as filename
    
    * (save) use ros time
    
    * (save) fix suppression of warnings (#79)
    
    * (save) fix suppression of warnings
    
    * (save) small cleanup
    
    * (save) use milliseconds
    
    * (save) use ros time milliseconds
    
    * (save) small improvement
    
    ---------
    
    Co-authored-by: Matthijs van der Burgh <MatthijsBurgh@outlook.com>

--- rgbd/tools/save.cpp
+++ rgbd/tools/save.cpp
@@ -9,9 +9,11 @@
 
 #include "rgbd/client.h"
 #include "rgbd/image.h"
+#include "rgbd/serialization.h"
 
-#include <rgbd/serialization.h>
+#include <ctime>
 #include <fstream>
+#include <iostream>
 
 int main(int argc, char **argv)
 {
@@ -22,8 +24,6 @@ int main(int argc, char **argv)
     float rate = 30;
     nh_private.getParam("rate", rate);
 
-    std::string file_name = "rgbd_image";
-
     rgbd::Client client;
     client.initialize(ros::names::resolve("rgbd"));
 
@@ -32,6 +32,7 @@ int main(int argc, char **argv)
     ros::WallTime last_master_check = ros::WallTime::now();
 
     ros::Rate r(rate);
+    char key_pressed;
     while (ros::ok())
     {
         if (ros::WallTime::now() >= last_master_check + ros::WallDuration(1))
@@ -43,19 +44,49 @@ int main(int argc, char **argv)
                 return 1;
             }
         }
-        if (client.nextImage(image))
-        {
-            // write
-            std::ofstream f_out;
-            f_out.open(file_name.c_str(), std::ifstream::binary);
-            tue::serialization::OutputArchive a_out(f_out);
-            rgbd::serialize(image, a_out);
-            f_out.close();
+        ROS_INFO("Press s to save and q to exit.");
 
-            ROS_INFO("Image stored to disk.");
+        #pragma GCC diagnostic push
+        #pragma GCC diagnostic ignored "-Wunused-result"
+        system ("/bin/stty raw");
+        key_pressed = getchar();
+        system ("/bin/stty cooked");
+        #pragma GCC diagnostic pop
+        
+        if (key_pressed == 's')
+        {
+            if (client.nextImage(image))
+            {
+                std::stringstream ss;
+                ss << "image_";
+                double sec;
+                const double fractional = std::modf(image.getTimestamp(), &sec);
+                const std::time_t time = sec;
+                ss << std::put_time(std::localtime(&time), "%Y-%m-%d_%H.%M.%S");
+                ss << "." << std::setw(6) << std::setfill('0') << static_cast<int32_t>(fractional*1e6);
+                ss << ".rgbd";
+                const std::string file_name = ss.str();
+
+                std::ofstream f_out;
+                f_out.open(file_name.c_str(), std::ifstream::binary);
+                try
+                {
+                    tue::serialization::OutputArchive a_out(f_out);
+                    rgbd::serialize(image, a_out);
+                    ROS_INFO_STREAM("Written image to '" << file_name << "'");
+                }
+                catch (const std::exception& e) // caught by reference to base
+                {
+                    ROS_ERROR_STREAM("Error while writing to '" << file_name << "':\n" << e.what());
+                }
+                f_out.close();
+            }
+        }
+        else if (key_pressed == 'q')
+        {
+            ROS_INFO("Exiting");
             return 0;
         }
-
         r.sleep();
     }
 
