06e4578830683a3f0f70f42a8c730d90b5a338f3
Matthijs van der Burgh <matthijs.van.der.burgh@vbti.nl>
2026-06-03 13:33:17 +0200

    Fix missing spin in 3d roi node

--- rgbd/src/get_3d_point_from_image_roi_node.cpp
+++ rgbd/src/get_3d_point_from_image_roi_node.cpp
@@ -123,6 +123,7 @@ int main(int argc, char **argv)
                 g_last_images_.push_back(std::make_shared<rgbd::Image>(image));
             }
         }
+        rclcpp::spin_some(node);
         r.sleep();
     }
 
